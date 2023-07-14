00d7b498cce87ec951235ee86900ef88e586318a
Liudmila <kanga_learn@mail.ru>
2023-07-14 23:09:03 +0800

    Добавлена программа

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string[] array1 = new string[7] { "ljkk", "qrq", "frog", "gua", "krpkrp", "ytr", "yud" };
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int k = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[k] = array1[i];
+            k++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
